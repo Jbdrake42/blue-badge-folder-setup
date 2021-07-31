584178ec910552980439f9b447c38dc0eafcebea
James Drake <jbdrake42@gmail.com>
2021-07-31 14:15:23 -0400

    updates yo

--- classroom-challenges/practice-server/app.js
+++ classroom-challenges/practice-server/app.js
@@ -1,18 +1,17 @@
 let express = require('express');
 const app = express()
-let journal = require('./controllers/journalController');
-let user = require('./controllers/userController')
+
+
 const sequelize = require("./db");
-app.use("/test", function(req,res){
-    res.send('this is a message from the test endpoint on the server!')
-} );
-
-sequelize.sync()
-
 app.use(express.json())
-app.use('/user', user)
 
-app.use("/journal", journal )
+let calculaterController = require('./controllers/calculaterController')
+sequelize.sync()
+app.use('/calculator', calculaterController)
+
+
+
+
 
 app.listen(3000, function(){
     console.log("the app is listening on port 3000")
